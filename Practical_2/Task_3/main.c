c47e25839559d1a6b1dcb4262d34bca112df304e
Ashley Kapaso <ashleykapaso@gmail.com>
2023-08-22 10:56:40 +0200

    impleament task 3

--- Practical_2/Task_3/main.c
+++ Practical_2/Task_3/main.c
@@ -3,7 +3,7 @@
 
 extern char** populateMatrix();
 extern char encryptChar(char**, char, char);
-//extern char* encryptString(char**, char*, char*);
+extern char* encryptString(char**, char*, char*);
 
 int main()
 {
@@ -16,7 +16,7 @@ int main()
         printf("\n");
     }*/
 
-    char input = 'B';
+    /*char input = 'B';
     char key = 'Z';
     printf("%c - %c -> %c\n", input, key, encryptChar(matrix, input, key));
     input = 'Z';
@@ -32,61 +32,61 @@ int main()
     for (int i = 0; i < 26; ++i) {
         free(matrix[i]);
     }
-    free(matrix);
+    free(matrix);*/
     
-    /*char* output = 0;
+    char* output = 0;
 
     char pt[50];
     char kw[50];
 
-    sprintf(pt, "ATTACK AT DAWN");
+    /*sprintf(pt, "ATTACK AT DAWN");
     sprintf(kw, "LEMON");
     printf("%s", pt);
     printf(" -- %s --> ", kw);
     output = encryptString(0, pt, kw);
-    printf("%s\n", output);
+    printf("%s\n", output);*/
 
     sprintf(pt, "OHWHYOHWHYDOWEWAIT");
     sprintf(kw, "POTATO");
     printf("%s", pt);
     printf(" -- %s --> ", kw);
-    output = encryptString(0, pt, kw);
+    output = encryptString(matrix, pt, kw);
     printf("%s\n", output);
 
     sprintf(pt, "SOMEEXSTRASPACESINBETWEENLOL");
     sprintf(kw, "SPACES");
     printf("%s", pt);
     printf(" -- %s --> ", kw);
-    output = encryptString(0, pt, kw);
+    output = encryptString(matrix, pt, kw);
     printf("%s\n", output);
 
     sprintf(pt, "SOMEEXTRASPACESATTHEBACK");
     sprintf(kw, "EXTRA");
     printf("%s", pt);
     printf(" -- %s --> ", kw);
-    output = encryptString(0, pt, kw);
+    output = encryptString(matrix, pt, kw);
     printf("%s\n", output);
 
     sprintf(pt, "EXTRASPACESATTHEFRONT");
     sprintf(kw, "COSTWOEIGHTFOUR");
     printf("%s", pt);
     printf(" -- %s --> ", kw);
-    output = encryptString(0, pt, kw);
+    output = encryptString(matrix, pt, kw);
     printf("%s\n", output);
 
     sprintf(pt, "EXTRASPACESEVERYWHERE");
     sprintf(kw, "ONETWOTHREE");
     printf("%s", pt);
     printf(" -- %s --> ", kw);
-    output = encryptString(0, pt, kw);
+    output = encryptString(matrix, pt, kw);
     printf("%s\n", output);
 
     sprintf(pt, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     sprintf(kw, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     printf("%s", pt);
     printf(" -- %s --> ", kw);
-    output = encryptString(0, pt, kw);
-    printf("%s\n", output);*/
+    output = encryptString(matrix, pt, kw);
+    printf("%s\n", output);
 
     return 0;
 }
